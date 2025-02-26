b5150ca8765b7f5a6faf858b89c31ff8ced6e845
JerodenExter <exte0005@hz.nl>
2025-02-26 10:45:01 +0100

    weekly 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,138 +1,27 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layout')
+@section('title', 'home')
+@section('content')
+    <img src="../img/logos/hz-logo.png" alt="hz logo">
+    <article>
+        <h1>Jero den Exter</h1>
+    </article>
+    <article>
+        <h1>Waarom heb ik voor deze opleiding gekozen?</h1>
+        <ul>
+            <li>Ik heb voor <a target="_blank" href="https://hz.nl/opleidingen/hbo-ict">deze opleiding</a> gekozen omdat ik graag beter wil leren programmeren. Het lijkt mij leuk om dit goed te kunnen en ook is het heel handig voor in de toekomst. </li>
+            <li>Je kan veel nieuwe mensen leren kennen en nieuwe vrienden maken op deze opleiding.</li>
+            <li>Je kan een leuke baan krijgen die goed betaald wordt en leuk is.</li>
+            <li>Informatica op <a target="_blank" href="https://www.lodewijkcollege.nl/">mijn middelbare school</a> vond ik leuk en deze opleiding heeft daar mee te maken.</li>
+            <li>Andere opleidingen leken mij iets minder dan deze en ik was laat met kiezen ik wilde namelijk als eerst de leraren opleiding doen maar dat kon niet.</li>
+        </ul>
+    </article>
+    <article>
+        <br></br>
+        <h1>Motivatie</h1>
+        <p>Mijn motivatie is het diploma dat je krijgt als je de studie afrond. Hier kan ik beter werk mee krijgen dat goed verdient en ook leuk is. Het werk is goed voor mijn toekomst en ook lijkt mij het leuk om te kunnen programmeren en dat helpt met mijn motivatie.
+            Op de middelbare school heb ik informatica gevolgd en ik vond dit vak best leuk. Ik denk dat ik daarom ook deze studie wel leuk zal vinden en dit als motivatie kan gebruiken.  </p>
+    </article>
+    <img src="../img/school.jpg" alt="foto school" class="fotoVanSchool">
+@endsection
 
-    <title>HZ HBO-ICT</title>
 
-    {{-- Compiled assets --}}
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-</head>
-<body>
-{{-- Navigation bar --}}
-<nav class="navbar is-primary  has-text-white" >
-    <div class="container">
-        <div class="navbar-brand">
-            <a href="/" class="navbar-item">
-                <strong><i class="fas fa-graduation-cap"></i> HZ</strong>
-            </a>
-            <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navMenu">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-            </a>
-        </div>
-        <div class="navbar-menu" id="navMenu">
-            <div class="navbar-start">
-                <a href="{{ route('home') }}"
-                   class="navbar-item {{ Request::route()->getName() === 'home' ? "is-active" : "" }}">
-                    Home
-                </a>
-            </div>
-        </div>
-    </div>
-</nav>
-
-{{-- Content --}}
-<section class="hero  is-medium  is-bold is-primary">
-    <div class="hero-body" style="
-            background: url('https://www.hz.nl/imager/uploads/images/3.-Werk-en-studie/Headers/docent-coacht-studenten-003_c8fa470484be7b69be5daae77a1602c5.jpg') no-repeat center center;
-            background-size: cover;"
-    ></div>
-</section>
-
-<section class="section">
-    <div class="container">
-        <div class="columns">
-
-            <div class="column is-8-desktop is-12-tablet">
-
-                <div class="content">
-                    <h1>Welcome to the HZ HBO-ICT template app.</h1>
-                    <p>
-                        Posuere porttitor natoque velit duis penatibus fermentum dignissim ut? Vel vel mi purus
-                        tempor nec conubia platea venenatis. Mauris pharetra auctor magnis, vehicula integer risus
-                        taciti gravida semper fames! Eu fermentum lorem accumsan litora. Consequat dapibus interdum
-                        primis lorem. Convallis integer mi suscipit tempor. Ad tincidunt placerat at. Sagittis
-                        pulvinar consectetur commodo, placerat varius sociosqu egestas felis! Curae; dictumst porta
-                        tempus. Nisi nec morbi netus euismod egestas proin sed tempor. Nam feugiat ante ante.
-                        Conubia vehicula tincidunt facilisis quisque risus senectus convallis. Eget at feugiat vel
-                        nisi. Tortor, facilisis neque elementum ultricies blandit amet orci dictumst eu mi molestie.
-                        Libero vulputate porta proin volutpat suspendisse aenean aenean facilisi ut primis!
-                        Venenatis elementum auctor neque urna et facilisis vulputate erat lorem habitasse libero!
-                        Risus ornare quam rutrum praesent blandit congue aliquet mauris.
-                    </p>
-                    <p>
-                        Elementum orci eget vel adipiscing tempor malesuada? Sollicitudin euismod nunc feugiat
-                        accumsan accumsan condimentum nulla pellentesque sagittis habitasse suspendisse praesent?
-                        Lectus proin justo vulputate tristique duis metus, est pellentesque blandit quam pharetra.
-                        Maecenas tincidunt litora mauris mollis ornare dictum nec placerat lectus massa lobortis
-                        auctor. Hac feugiat dolor, ac ridiculus mi adipiscing aptent elementum suspendisse augue
-                        rutrum! Habitasse a consequat ornare dictumst integer dis porta.
-                    </p>
-                    <p>
-                        Blandit mollis felis gravida sem ad venenatis ut a lacus! Magna odio netus, torquent rhoncus
-                        fames velit. Adipiscing libero, class cursus ipsum penatibus enim mollis netus. Sociis
-                        luctus eros sapien platea parturient fringilla. Senectus purus tincidunt laoreet purus
-                        praesent. Accumsan pulvinar tincidunt gravida malesuada senectus proin, elit cubilia
-                        lobortis quisque tellus? Torquent dis natoque sapien natoque sagittis conubia pulvinar risus
-                        elementum hac adipiscing arcu. Sem nisl fusce phasellus adipiscing pretium eget hendrerit
-                        proin non. Vestibulum lacus magna, commodo volutpat.
-                    </p>
-                    <p>
-                        Vivamus ridiculus amet arcu. Magnis venenatis vehicula venenatis molestie molestie proin sed
-                        blandit, inceptos volutpat. Auctor auctor massa sodales ipsum. Congue est quisque porta?
-                        Porta phasellus sem torquent ad feugiat sollicitudin nisi tempor tortor habitasse. Eros
-                        condimentum enim blandit pulvinar. Justo aenean, taciti curae; diam volutpat cubilia ante
-                        vestibulum. Augue penatibus phasellus inceptos tortor convallis ridiculus at ad. Nisl
-                        curabitur vel gravida, montes sit velit pellentesque. Sem, dapibus maecenas urna lectus
-                        mauris quisque nibh accumsan amet curabitur egestas dolor. Sagittis montes magnis quam
-                        fringilla accumsan bibendum. Nisl, a laoreet himenaeos sapien lorem quisque taciti lacus
-                        elit mauris. Mollis eleifend montes commodo? Accumsan nullam venenatis malesuada netus metus
-                        sociosqu magnis velit. Rhoncus lacus tempus praesent fermentum netus ullamcorper dolor
-                        integer nulla.
-                    </p>
-                    <p>
-                        Habitasse suspendisse egestas vestibulum pellentesque per leo enim metus donec ad. Hac metus
-                        convallis nibh eget, parturient dis elementum posuere. Felis, ultricies fusce est cubilia
-                        facilisis odio id velit dapibus suspendisse. Erat praesent nullam aliquam. Rutrum commodo
-                        eget malesuada per montes, curae; senectus convallis sociis per. Risus mattis justo vivamus
-                        lorem bibendum aenean eleifend facilisi, suscipit consectetur libero litora. Interdum?
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-
-{{-- Footer --}}
-<footer class="footer">
-    <div class="container">
-        <div class="columns is-multiline">
-
-            <div class="column has-text-centered">
-                <div>
-                    <a href="/" class="link">Home</a>
-                </div>
-            </div>
-
-            <div class="column has-text-centered">
-                <div>
-                    <a href="https://opensource.org/licenses/MIT" class="link">
-                        <i class="fa fa-balance-scale" aria-hidden="true"></i> License: MIT
-                    </a>
-                </div>
-            </div>
-
-        </div>
-
-        <div class="content is-small has-text-centered">
-            <p class="">Theme built by <a href="https://www.csrhymes.com">C.S. Rhymes</a> | adapted by <a href="https://github.com/dwaard">BugSlayer</a></p>
-            <p>PROJECT FOOTER HERE</p>
-        </div>
-    </div>
-</footer>
-
-</body>
-</html>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\BlogController;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+// Home & Static Pages
+Route::get('/', [PageController::class, 'home'])->name('home');
+Route::get('/home', [PageController::class, 'home']);
+Route::get('/profile', [PageController::class, 'profile'])->name('profile');
+Route::get('/faq', [PageController::class, 'faq'])->name('faq');
+Route::get('/dashboard', [PageController::class, 'dashboard'])->name('dashboard');
 
+// Blog Routes
+Route::get('/blog', [BlogController::class, 'blog'])->name('blog');
+Route::get('/blog/1', [BlogController::class, 'blog1'])->name('blog1');
+Route::get('/blog/2', [BlogController::class, 'blog2'])->name('blog2');
+Route::get('/blog/3', [BlogController::class, 'blog3'])->name('blog3');
+Route::get('/blog/4', [BlogController::class, 'blog4'])->name('blog4');
+Route::get('/blog/5', [BlogController::class, 'blog5'])->name('blog5');
